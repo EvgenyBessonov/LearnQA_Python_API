936eac1e9e89a30133aeee281b797127971cae1e
EvgenyBessonov <84249956+EvgenyBessonov@users.noreply.github.com>
2021-06-29 13:28:46 +0300

    Lesson 2 - task 4

---
+++ token HW.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+
+test_url = "https://playground.learnqa.ru/ajax/api/longtime_job"
+
+# task 1
+r1 = requests.get(test_url)
+token = r1.json()["token"]
+print(r1.status_code, r1.text)
+
+# task 2
+payload = {"token": token}
+r2 = requests.get(test_url, params=payload)
+assert r2.json()["status"] == "Job is NOT ready"
+print(r2.status_code)
+
+# task 3
+time.sleep(r1.json()["seconds"])
+
+# task 4
+payload = {"token": token}
+r3 = requests.get(test_url, params=payload)
+assert r3.json()["status"] == "Job is ready"
+assert "result" in r3.json()
+
+
+
